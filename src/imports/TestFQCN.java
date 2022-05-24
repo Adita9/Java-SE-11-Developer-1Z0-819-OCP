6edfe9084778f38e68cab3086cdda6d0a38ff512
adrianneagoe <adrian.neagoe@1und1.de>
2022-05-24 22:38:16 +0200

    Section 1 - packages and imports

---
+++ src/imports/TestFQCN.java
@@ -0,0 +1,8 @@
+package imports;
+
+public class TestFQCN {
+    public static void main(String [] args){
+        System.out.println("We are testing ways to import classes");
+        garden.vegetable.VineVegetable.main(args);
+    }
+}
---
+++ src/imports/TestImport.java
@@ -0,0 +1,14 @@
+package imports;
+
+import garden.vegetable.*;
+import nursery.vegetable.*;
+
+public class TestImport {
+    public static void main(String [] args){
+        garden.vegetable.VineVegetable.main(args);
+        nursery.vegetable.VineVegetable.main(args);
+    }
+}
+class VineVegetable{
+
+}
